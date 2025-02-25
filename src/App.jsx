2fd9744c54256117d7ba8fc89ac07df7c99bdadc
AppVeyor <ci@appveyor.com>
2025-02-25 17:29:55 +0300

    add file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Portfolio from './components/filter/Portfolio'
+import Store from './components/layouts/Store'
+
+function App() {
+  return (
+    <>
+      <Portfolio />
+      <Store />
+    </>
+  )
+}
+
+export default App
